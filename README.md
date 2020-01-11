64ffa95c271558593425014ad75412eef03bb2c8
Takuma <mukudori09@gmail.com>
2020-01-12 02:36:15 +0900

    Initial commit.

---
+++ README.md
@@ -0,0 +1,15 @@
+## Usage
+
+```
+$ npm install
+```
+
+then
+
+```
+$ npm run serve
+```
+
+then access http://localhost:8080/
+
+Tweak some settings in src/index.js to see how time-to-color-converter works better.
---
+++ src/index.js
@@ -0,0 +1,47 @@
+import convert from 'time-to-color-converter'; // If you want to use import statement.
+// const convert = require('time-to-color-converter'); // If you want to use requre function.
+
+const body = document.getElementsByTagName('body')[0];
+const dateDisplay = document.getElementsByClassName('date')[0];
+const colorDisplay = document.getElementsByClassName('color')[0];
+
+const startupDate = new Date();
+
+animate();
+
+function animate() {
+  requestAnimationFrame(animate);
+
+
+  // Some static points in time for example
+  // const date = new Date(2020, 12 - 1, 21, 6, 0, 0, 0); // winter solstice in 2020
+  // const date = new Date(2020, 6 - 1, 21, 6, 0, 0, 0); // summer solstice in 2020
+  
+
+  // Start animation from any point of time
+  const startPointInTime = new Date(2020, 1 - 1, 1, 0, 0, 0, 0);
+  // Or just from now
+  // const startPointInTime = new Date();
+
+  // Fast Forward
+  const timesFaster = 60 * 60 * 24 / 2; // A day in 2 seconds
+
+  // Fast Forwarded animation from startup
+  const date = new Date(startPointInTime.getTime() + (new Date() - startupDate) * timesFaster);
+
+
+  const { h, s, l } = convert(date);
+  const colorStrInHls = `hsl(${h}, ${s}%, ${l}%)`;
+  body.style.backgroundColor = colorStrInHls;
+  dateDisplay.innerHTML = `Date is ${date}`;
+  colorDisplay.innerHTML = `Color is #${colorStrInHls}`;
+  const darkInHls = 'hsl(0, 0%, 25%)';
+  const lightInHls = 'hsl(0, 0%, 75%)';
+  if ( l > 50 ) {
+    dateDisplay.style.color = darkInHls;
+    colorDisplay.style.color = darkInHls;
+  } else {
+    dateDisplay.style.color = lightInHls;
+    colorDisplay.style.color = lightInHls;
+  }
+}
